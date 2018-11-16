f984e54d1b823cec737a798511bb6f5bf35448a1
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2018-11-16 11:56:09 -0500

    Added missing import

--- python/minc2_simple/__init__.py
+++ python/minc2_simple/__init__.py
@@ -1,5 +1,5 @@
-from .minc2_simple import minc2_error,minc2_transform_parameters,minc2_file,minc2_xfm,minc2_tags
+from .minc2_simple import minc2_error,minc2_transform_parameters,minc2_file,minc2_xfm,minc2_tags,minc2_dim
 
-__all__ = ['minc2_error','minc2_transform_parameters','minc2_file','minc2_xfm','minc2_tags']
+__all__ = ['minc2_error','minc2_transform_parameters','minc2_file','minc2_xfm','minc2_tags','minc2_dim']
 
 
