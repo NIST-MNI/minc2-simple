e7590254fc0c0a29fcce12e029b02411e5535886
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2019-11-19 18:03:50 -0500

    Extended torch example

--- example/torch/minc_torch_segment.cpp
+++ example/torch/minc_torch_segment.cpp
@@ -20,7 +20,10 @@ void show_usage(const char *name)
     << "\t--channels <n> add more input channels, set them to 38.81240207 for now" << std::endl
     << "\t--mask <file> use binary mask to restrict application of the network (TODO)" << std::endl
     << "\t--verbose be verbose" << std::endl
-    << "\t--clobber clobber the output files" << std::endl;
+    << "\t--clobber clobber the output files" << std::endl
+    << "\t--patch <n> patch size" << std::endl
+    << "\t--stride <n> stride, should be less then patch" << std::endl
+    << "\t--crop <n>   crop n voxels from the edge, should be less then patch/2" << std::endl;
 }
 
 
@@ -31,14 +34,21 @@ int main(int argc,char **argv)
     std::string mask_f;
     int channels=1;
     int c;
+    int patch_sz = 80;
+    int crop = 4;
+    int stride = (patch_sz-crop*2)/2;
+
     // read the arguments
     static struct option long_options[] =
     {
-        {"verbose", no_argument, &verbose, 1},
-        {"quiet",   no_argument, &verbose, 0},
-        {"clobber", no_argument, &clobber, 1},
-        {"mask",    required_argument, 0, 'm'},
-        {"channels",    required_argument, 0, 'c'},
+        {"verbose",   no_argument, &verbose, 1},
+        {"quiet",     no_argument, &verbose, 0},
+        {"clobber",   no_argument, &clobber, 1},
+        {"mask",      required_argument, 0, 'm'},
+        {"channels",  required_argument, 0, 'c'},
+        {"patch",     required_argument, 0, 'p'},
+        {"stride",    required_argument, 0, 's'},
+        {"crop",      required_argument, 0, 'R'},
         {0, 0, 0, 0}
     };
 
@@ -60,6 +70,15 @@ int main(int argc,char **argv)
         case 'c':
             channels=atoi(optarg);
             break;
+        case 'p':
+            patch_sz=atoi(optarg);
+            break;
+        case 's':
+            stride=atoi(optarg);
+            break;
+        case 'R':
+            crop=atoi(optarg);
+            break;
         case 'm':
             mask_f=optarg;
             break;
@@ -80,11 +99,11 @@ int main(int argc,char **argv)
     try {
         int ndim;
         int nelement;
-        const int patch_sz = 80;
-        const int stride = patch_sz/2;
+        int patch_sz_eff = patch_sz - crop*2;
         struct minc2_dimension * _dims;
         struct minc2_dimension * store_dims;
         torch::NoGradGuard no_grad; // disable gradient calculation (?)
+
         // Deserialize the ScriptModule from a file using torch::jit::load().
         torch::jit::script::Module module = torch::jit::load(argv[optind]);
         module.eval();
@@ -164,26 +183,33 @@ int main(int argc,char **argv)
         }
 
 
-        // TODO: make patches overlap, and then aggregate results
-        for(int z=0; z< (_dims[0].length - _dims[0].length%patch_sz); z+=stride)
+        // TODO: queue patches in the batch dimension?
+        for(int z=0; z< ceil((double)_dims[0].length / stride ); z ++)
         {
-            for(int y=0; y< (_dims[1].length- _dims[1].length%patch_sz); y+=stride)
+            for(int y=0; y< ceil((double)_dims[1].length / stride ); y ++)
             {
-                for(int x=0; x< (_dims[2].length- _dims[2].length%patch_sz); x+=stride)
+                for(int x=0; x< ceil((double)_dims[2].length/stride ); x ++)
                 {
-                    int _x = ( x + patch_sz) > _dims[2].length ? _dims[2].length - patch_sz:x;
-                    int _y = ( y + patch_sz) > _dims[1].length ? _dims[1].length - patch_sz:y;
-                    int _z = ( z + patch_sz) > _dims[0].length ? _dims[0].length - patch_sz:z;
+                    torch::NoGradGuard no_grad;
+                    int _x = x*stride;
+                    int _y = y*stride;
+                    int _z = z*stride;
+                    _x = ( _x + patch_sz) > _dims[2].length ? _dims[2].length - patch_sz:_x;
+                    _y = ( _y + patch_sz) > _dims[1].length ? _dims[1].length - patch_sz:_y;
+                    _z = ( _z + patch_sz) > _dims[0].length ? _dims[0].length - patch_sz:_z;
 
                     std::cout << _x << "," << _y << "," << _z <<std::endl;
                     // Create a vector of inputs.
                     std::vector<torch::jit::IValue> inputs;
+
                     auto inp = input.slice(2,_z,_z+patch_sz).slice(3,_y,_y+patch_sz).slice(4, _x, _x+patch_sz);
                     // extract patch
                     inputs.push_back( inp );
 
                     // Execute the model and turn its output into a tensor.
-                    auto out = torch::log_softmax(module.forward(inputs).toTensor(), /*dim=*/1);
+                    auto out = torch::log_softmax( module.forward(inputs).toTensor(), /*dim=*/1 );
+
+                    // drop batch dimension
                     out = out.squeeze(0);
 
                     if(!output.defined()) // neeed to allocate
@@ -193,30 +219,25 @@ int main(int argc,char **argv)
                         std::cout<<"Allocating output:" << out.size(0) << ","<< _dims[0].length << ","<< _dims[1].length << "," << _dims[2].length << std::endl;
                         std::cout<<"Allocating ones:"   << out.size(0) << ","<< patch_sz << "," << patch_sz << "," << patch_sz << std::endl;
 
-                        output  = torch::zeros( {out.size(0), _dims[0].length, _dims[1].length, _dims[2].length });
-                        weights = torch::zeros( {out.size(0), _dims[0].length, _dims[1].length, _dims[2].length });
-                        ones    = torch::ones(  {out.size(0), patch_sz, patch_sz, patch_sz } );
+                        output  = torch::zeros( { out.size(0), _dims[0].length, _dims[1].length, _dims[2].length });
+                        weights = torch::zeros( { out.size(0), _dims[0].length, _dims[1].length, _dims[2].length });
+                        ones    = torch::ones(  { out.size(0), patch_sz_eff, patch_sz_eff, patch_sz_eff } );
                     }
 
-                    output.slice(1, _z,_z+patch_sz).slice(2,_y,_y+patch_sz).slice(3,_x,_x+patch_sz)  += out;
-                    weights.slice(1,_z,_z+patch_sz).slice(2,_y,_y+patch_sz).slice(3,_x,_x+patch_sz) += ones;
+                    output.slice(1, _z+crop ,_z+crop+patch_sz_eff).slice(2,_y+crop,_y+crop+patch_sz_eff).slice(3,_x+crop,_x+crop+patch_sz_eff) += out.slice(1,crop,crop+patch_sz_eff).slice(2,crop,crop+patch_sz_eff).slice(3,crop,crop+patch_sz_eff);
+                    weights.slice(1,_z+crop ,_z+crop+patch_sz_eff).slice(2,_y+crop,_y+crop+patch_sz_eff).slice(3,_x+crop,_x+crop+patch_sz_eff) += ones;
 
-
-                    /// DEBUG
-                    //break;
                 }
-
-                /// DEBUG
-                //break;
             }
-            /// DEBUG
-            //break;
         }
+        // HACK to avoid devide by zero on the border
+        weights.masked_fill(weights<1.0,1.0);
 
         output/=weights; // normalize
+        output.exp_(); // apply exponent
 
         if(debug_output)
-            output_d = output.slice(0,0,1).squeeze(0);
+            output_d = weights.slice(0,0,1).squeeze(0);
 
         // extract prediction
         auto out_ = output.argmax(0).to(torch::kU8); 
@@ -239,6 +260,7 @@ int main(int argc,char **argv)
 
         minc2_close(h);
         minc2_close(o);
+
         if(debug_output)
             minc2_close(oo);
         /*deallocate*/
